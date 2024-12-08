a09aec6db84f30149d4bced31205167d76c038dc
appleboy <appleboy.tw@gmail.com>
2024-12-08 11:11:16 +0800

    feat(auth): implement go-jira CLI with authentication and user info
    - Add a new main.go file for the go-jira command-line tool
    - Implement version flag handling in main.go
    - Load environment variables from a specified file in main.go
    - Configure HTTP client with optional basic or bearer authentication in main.go
    - Fetch and display Jira user information in main.go
    - Add a utility function to retrieve environment variable values in util.go
    - Create a go.mod file with module dependencies and replacements
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

---
+++ cmd/go-jira/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"net/http"
+
+	jira "github.com/andygrunwald/go-jira"
+	"github.com/joho/godotenv"
+)
+
+var (
+	Version     string
+	Commit      string
+	showVersion bool
+)
+
+func main() {
+	var envfile string
+	flag.StringVar(&envfile, "env-file", ".env", "Read in a file of environment variables")
+	flag.BoolVar(&showVersion, "version", false, "Show version")
+	flag.Parse()
+
+	if showVersion {
+		fmt.Printf("Version: %s Commit: %s\n", Version, Commit)
+		return
+	}
+
+	_ = godotenv.Load(envfile)
+
+	baseURL := getGlobalValue("base_url")
+	insecure := getGlobalValue("insecure")
+	username := getGlobalValue("username")
+	password := getGlobalValue("password")
+	token := getGlobalValue("token")
+
+	var httpTransport *http.Transport = nil
+	var httpClient *http.Client = nil
+
+	if insecure == "true" {
+		httpTransport = &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+	}
+
+	if username != "" && password != "" {
+		auth := jira.BasicAuthTransport{
+			Username:  username,
+			Password:  password,
+			Transport: httpTransport,
+		}
+		httpClient = auth.Client()
+	}
+
+	if token != "" {
+		auth := jira.BearerAuthTransport{
+			Token:     token,
+			Transport: httpTransport,
+		}
+		httpClient = auth.Client()
+	}
+
+	jiraClient, err := jira.NewClient(httpClient, baseURL)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	user, _, err := jiraClient.User.GetByUsernameWithContext(context.Background(), username)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	fmt.Printf("Reporter: %s\n", user.DisplayName)
+}
---
+++ cmd/go-jira/util.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+	"strings"
+)
+
+func getGlobalValue(key string) string {
+	key = strings.ToUpper(key) // Convert key to uppercase
+
+	// Check if there is an environment variable with the format "INPUT_<KEY>"
+	if value := os.Getenv("INPUT_" + key); value != "" {
+		return value // Return the value of the "INPUT_<KEY>" environment variable
+	}
+
+	// If the "INPUT_<KEY>" environment variable doesn't exist or is empty,
+	// return the value of the "<KEY>" environment variable
+	return os.Getenv(key)
+}
