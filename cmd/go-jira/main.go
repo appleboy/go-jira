a9a0964ffc7c2510e01ae11f5292a4bb7ed9c2d6
appleboy <appleboy.tw@gmail.com>
2025-01-01 15:53:02 +0800

    refactor: refactor configuration handling and modularize Jira client functions
    - Refactor to use a `Config` struct for configuration values
    - Replace individual global value retrievals with `loadConfig` function
    - Add `createHTTPClient` function to handle HTTP client creation based on `Config`
    - Add `getUser`, `getAuthorUser`, `getResolutionID`, `processTransitions`, and `addComments` functions for better modularity
    - Replace direct variable usage with `config` struct fields
    - Add function to deduplicate issue keys based on a given pattern
    - Improve error handling and logging for Jira client operations
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- cmd/go-jira/main.go
+++ cmd/go-jira/main.go
@@ -39,197 +39,49 @@ func main() {
 
 	_ = godotenv.Load(envfile)
 
-	baseURL := getGlobalValue("base_url")         // jira base url
-	insecure := getGlobalValue("insecure")        // skip verify ssl certificate
-	username := getGlobalValue("username")        // jira username
-	password := getGlobalValue("password")        // use token instead of username and password
-	token := getGlobalValue("token")              // token for authentication
-	ref := getGlobalValue("ref")                  // git tag or branch name
-	issueFormat := getGlobalValue("issue_format") // issue regular expression pattern
-	toTransition := getGlobalValue("transition")  // move issue to a specific status
-	resolution := getGlobalValue("resolution")    // set resolution when moving issue to a specific status
-	comment := getGlobalValue("comment")          // add comment when moving issue to a specific status
-	author := getGlobalValue("author")            // author of the comment
-	debug := getGlobalValue("debug")              // enable debug mode
-
-	if debug == "true" {
-		_ = godump.Dump(ref)
-		_ = godump.Dump(issueFormat)
-		_ = godump.Dump(toTransition)
-		_ = godump.Dump(resolution)
+	config := loadConfig()
+	if config.debug == "true" {
+		_ = godump.Dump(config.ref)
+		_ = godump.Dump(config.issueFormat)
+		_ = godump.Dump(config.toTransition)
+		_ = godump.Dump(config.resolution)
+		_ = godump.Dump(config.comment)
+		_ = godump.Dump(config.author)
 	}
 
-	var httpTransport *http.Transport = nil
-	var httpClient *http.Client = nil
-
-	if insecure == "true" {
-		slog.Warn("Skipping SSL certificate verification is insecure and not recommended")
-		httpTransport = &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // #nosec
-		}
-	}
-
-	if username != "" && password != "" {
-		auth := jira.BasicAuthTransport{
-			Username:  username,
-			Password:  password,
-			Transport: httpTransport,
-		}
-		httpClient = auth.Client()
-	}
-
-	if token != "" {
-		auth := jira.BearerAuthTransport{
-			Token:     token,
-			Transport: httpTransport,
-		}
-		httpClient = auth.Client()
-	}
-
-	jiraClient, err := jira.NewClient(httpClient, baseURL)
+	httpClient := createHTTPClient(config)
+	jiraClient, err := jira.NewClient(httpClient, config.baseURL)
 	if err != nil {
 		slog.Error("error creating jira client", "error", err)
 		return
 	}
 
-	user, _, err := jiraClient.User.GetSelfWithContext(context.Background())
+	user, err := getUser(jiraClient)
 	if err != nil {
 		slog.Error("error getting self", "error", err)
 		return
 	}
 
-	slog.Info("login account",
-		"displayName", user.DisplayName,
-		"email", user.EmailAddress,
-		"username", user.Name,
-	)
-
-	var authorUser *jira.User
-	if author != "" {
-		authorUser, _, err = jiraClient.User.GetByUsernameWithContext(context.Background(), author)
-		if err != nil {
-			slog.Error("error getting author", "error", err)
-			return
-		}
-		if authorUser != nil {
-			slog.Info("author account",
-				"displayName", authorUser.DisplayName,
-				"email", authorUser.EmailAddress,
-				"username", authorUser.Name,
-			)
-		}
+	authorUser, err := getAuthorUser(jiraClient, config.author)
+	if err != nil {
+		slog.Error("error getting author", "error", err)
+		return
 	}
 
-	// get resolution id
-	if resolution != "" {
-		resolutions, resp, err := jiraClient.Resolution.GetListWithContext(context.Background())
+	if config.resolution != "" {
+		config.resolution, err = getResolutionID(jiraClient, config.resolution)
 		if err != nil {
 			slog.Error("error getting resolution", "error", err)
 			return
 		}
-		if resp.StatusCode != http.StatusOK {
-			slog.Error("error getting resolution", "status", resp.Status)
-			return
-		}
-		for _, r := range resolutions {
-			if strings.EqualFold(r.Name, resolution) {
-				resolution = r.ID
-				break
-			}
-		}
 	}
 
-	if ref != "" && toTransition != "" {
-		issueKeys := getIssueKeys(ref, issueFormat)
-		for _, issueKey := range issueKeys {
-			issue, resp, err := jiraClient.Issue.GetWithContext(context.Background(), issueKey, &jira.GetQueryOptions{
-				Expand: "transitions",
-			})
-			if err != nil {
-				slog.Error("error getting issue", "issue", issueKey, "error", err)
-				continue
-			}
-			if resp.StatusCode != http.StatusOK {
-				slog.Error("error getting issue", "issue", issueKey, "status", resp.Status)
-				continue
-			}
-			slog.Info("issue info",
-				"key", issue.Key,
-				"summary", issue.Fields.Summary,
-				"current status", issue.Fields.Status.Name,
-			)
-			for _, transition := range issue.Transitions {
-				if !strings.EqualFold(transition.Name, toTransition) {
-					continue
-				}
-
-				input := &jira.TransitionPayloadInput{
-					TicketID:     issue.Key,
-					TransitionID: transition.ID,
-				}
-				if resolution != "" {
-					input.ResolutionID = convert.ToPtr(resolution)
-				}
-				resp, err := jiraClient.Issue.DoTransitionPayloadWithContext(
-					context.Background(),
-					input,
-				)
-				if err != nil {
-					slog.Error("error moving issue", "issue", issueKey, "error", err)
-					continue
-				}
-				if resp.StatusCode != http.StatusNoContent {
-					slog.Error("error moving issue", "issue", issueKey, "status", resp.Status)
-					continue
-				}
-				slog.Info("issue moved",
-					"key", issue.Key,
-					"summary", issue.Fields.Summary,
-					"transition", transition.Name,
-				)
-			}
-		}
+	if config.ref != "" && config.toTransition != "" {
+		processTransitions(jiraClient, config, user)
 	}
 
-	if comment != "" {
-		currentUser := user
-		if authorUser != nil {
-			currentUser = authorUser
-		}
-
-		issueKeys := getIssueKeys(ref, issueFormat)
-		for _, issueKey := range issueKeys {
-			slog.Info(
-				"author",
-				"displayName", currentUser.DisplayName,
-				"email", currentUser.EmailAddress,
-				"username", currentUser.Name,
-			)
-			comment, resp, err := jiraClient.Issue.AddCommentWithContext(
-				context.Background(),
-				issueKey,
-				&jira.Comment{
-					Name:         currentUser.Name,
-					Author:       *currentUser,
-					UpdateAuthor: *currentUser,
-					Body:         comment,
-				},
-			)
-			if err != nil {
-				slog.Error("error adding comment", "issue", issueKey, "error", err)
-				continue
-			}
-
-			if resp.StatusCode != http.StatusCreated {
-				body, _ := io.ReadAll(resp.Body)
-				slog.Error("error adding comment", "issue", issueKey, "status", resp.StatusCode, "body", string(body))
-				continue
-			}
-			slog.Info("comment added",
-				"issue", issueKey,
-				"comment", comment.Body,
-			)
-		}
+	if config.comment != "" {
+		addComments(jiraClient, config, user, authorUser)
 	}
 }
 
@@ -251,4 +103,209 @@ func getIssueKeys(ref, issueFormat string) []string {
 		issueKeys = append(issueKeys, match)
 	}
 	return issueKeys
+}
+
+type Config struct {
+	baseURL      string
+	insecure     string
+	username     string
+	password     string
+	token        string
+	ref          string
+	issueFormat  string
+	toTransition string
+	resolution   string
+	comment      string
+	author       string
+	debug        string
+}
+
+func loadConfig() Config {
+	return Config{
+		baseURL:      getGlobalValue("base_url"),
+		insecure:     getGlobalValue("insecure"),
+		username:     getGlobalValue("username"),
+		password:     getGlobalValue("password"),
+		token:        getGlobalValue("token"),
+		ref:          getGlobalValue("ref"),
+		issueFormat:  getGlobalValue("issue_format"),
+		toTransition: getGlobalValue("transition"),
+		resolution:   getGlobalValue("resolution"),
+		comment:      getGlobalValue("comment"),
+		author:       getGlobalValue("author"),
+		debug:        getGlobalValue("debug"),
+	}
+}
+
+func createHTTPClient(config Config) *http.Client {
+	var httpTransport *http.Transport = nil
+
+	if config.insecure == "true" {
+		slog.Warn("Skipping SSL certificate verification is insecure and not recommended")
+		httpTransport = &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // #nosec
+		}
+	}
+
+	if config.username != "" && config.password != "" {
+		auth := jira.BasicAuthTransport{
+			Username:  config.username,
+			Password:  config.password,
+			Transport: httpTransport,
+		}
+		return auth.Client()
+	}
+
+	if config.token != "" {
+		auth := jira.BearerAuthTransport{
+			Token:     config.token,
+			Transport: httpTransport,
+		}
+		return auth.Client()
+	}
+
+	return &http.Client{Transport: httpTransport}
+}
+
+func getUser(jiraClient *jira.Client) (*jira.User, error) {
+	user, _, err := jiraClient.User.GetSelfWithContext(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	slog.Info("login account",
+		"displayName", user.DisplayName,
+		"email", user.EmailAddress,
+		"username", user.Name,
+	)
+	return user, nil
+}
+
+func getAuthorUser(jiraClient *jira.Client, author string) (*jira.User, error) {
+	if author == "" {
+		return nil, nil
+	}
+
+	authorUser, _, err := jiraClient.User.GetByUsernameWithContext(context.Background(), author)
+	if err != nil {
+		return nil, err
+	}
+
+	if authorUser != nil {
+		slog.Info("author account",
+			"displayName", authorUser.DisplayName,
+			"email", authorUser.EmailAddress,
+			"username", authorUser.Name,
+		)
+	}
+	return authorUser, nil
+}
+
+func getResolutionID(jiraClient *jira.Client, resolution string) (string, error) {
+	resolutions, resp, err := jiraClient.Resolution.GetListWithContext(context.Background())
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("error getting resolution: %s", resp.Status)
+	}
+	for _, r := range resolutions {
+		if strings.EqualFold(r.Name, resolution) {
+			return r.ID, nil
+		}
+	}
+	return "", nil
+}
+
+func processTransitions(jiraClient *jira.Client, config Config, _ *jira.User) {
+	issueKeys := getIssueKeys(config.ref, config.issueFormat)
+	for _, issueKey := range issueKeys {
+		issue, resp, err := jiraClient.Issue.GetWithContext(context.Background(), issueKey, &jira.GetQueryOptions{
+			Expand: "transitions",
+		})
+		if err != nil {
+			slog.Error("error getting issue", "issue", issueKey, "error", err)
+			continue
+		}
+		if resp.StatusCode != http.StatusOK {
+			slog.Error("error getting issue", "issue", issueKey, "status", resp.Status)
+			continue
+		}
+		slog.Info("issue info",
+			"key", issue.Key,
+			"summary", issue.Fields.Summary,
+			"current status", issue.Fields.Status.Name,
+		)
+		for _, transition := range issue.Transitions {
+			if !strings.EqualFold(transition.Name, config.toTransition) {
+				continue
+			}
+
+			input := &jira.TransitionPayloadInput{
+				TicketID:     issue.Key,
+				TransitionID: transition.ID,
+			}
+			if config.resolution != "" {
+				input.ResolutionID = convert.ToPtr(config.resolution)
+			}
+			resp, err := jiraClient.Issue.DoTransitionPayloadWithContext(
+				context.Background(),
+				input,
+			)
+			if err != nil {
+				slog.Error("error moving issue", "issue", issueKey, "error", err)
+				continue
+			}
+			if resp.StatusCode != http.StatusNoContent {
+				slog.Error("error moving issue", "issue", issueKey, "status", resp.Status)
+				continue
+			}
+			slog.Info("issue moved",
+				"key", issue.Key,
+				"summary", issue.Fields.Summary,
+				"transition", transition.Name,
+			)
+		}
+	}
+}
+
+func addComments(jiraClient *jira.Client, config Config, user, authorUser *jira.User) {
+	currentUser := user
+	if authorUser != nil {
+		currentUser = authorUser
+	}
+
+	issueKeys := getIssueKeys(config.ref, config.issueFormat)
+	for _, issueKey := range issueKeys {
+		slog.Info(
+			"author",
+			"displayName", currentUser.DisplayName,
+			"email", currentUser.EmailAddress,
+			"username", currentUser.Name,
+		)
+		comment, resp, err := jiraClient.Issue.AddCommentWithContext(
+			context.Background(),
+			issueKey,
+			&jira.Comment{
+				Name:         currentUser.Name,
+				Author:       *currentUser,
+				UpdateAuthor: *currentUser,
+				Body:         config.comment,
+			},
+		)
+		if err != nil {
+			slog.Error("error adding comment", "issue", issueKey, "error", err)
+			continue
+		}
+
+		if resp.StatusCode != http.StatusCreated {
+			body, _ := io.ReadAll(resp.Body)
+			slog.Error("error adding comment", "issue", issueKey, "status", resp.StatusCode, "body", string(body))
+			continue
+		}
+		slog.Info("comment added",
+			"issue", issueKey,
+			"comment", comment.Body,
+		)
+	}
 }
